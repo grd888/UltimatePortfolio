46066cfb422ab4b34e9eef96b6e2b42548598048
Greg Delgado <grd888@gmail.com>
2023-02-20 20:32:44 +0800

    create CoreData model, DataController and initial NavigationSplitView

---
+++ UltimatePortfolio/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  UltimatePortfolio
+//
+//  Created by Greg Delgado on 2/19/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Detail")
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
--- UltimatePortfolio/UltimatePortfolioApp.swift
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -12,9 +12,15 @@ struct UltimatePortfolioApp: App {
     @State var dataController = DataController()
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, dataController.container.viewContext)
-                .environmentObject(dataController)
+            NavigationSplitView {
+                SidebarView()
+            } content: {
+                ContentView()
+            } detail: {
+                DetailView()
+            }
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
         }
     }
 }
