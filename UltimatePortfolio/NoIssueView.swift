402afe244d63b47d43034fe76d50145bf21fff66
Greg Delgado <grd888@gmail.com>
2023-04-23 11:13:56 +0800

    editing items

---
+++ UltimatePortfolio/NoIssueView.swift
@@ -0,0 +1,27 @@
+//
+//  NoIssueView.swift
+//  UltimatePortfolio
+//
+//  Created by Greg Delgado on 4/23/23.
+//
+
+import SwiftUI
+
+struct NoIssueView: View {
+    @EnvironmentObject var dataController: DataController
+    var body: some View {
+        Text("No Issue Selected")
+            .font(.title)
+            .foregroundStyle(.secondary)
+
+        Button("New Issue") {
+            // make a new issue
+        }
+    }
+}
+
+struct NoIssueView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoIssueView()
+    }
+}
